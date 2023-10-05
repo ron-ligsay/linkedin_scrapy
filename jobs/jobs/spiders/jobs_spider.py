6de2979db965e6dc73d0aa54d486f322db643ba4
ron-ligsay <akyligsay16@gmail.com>
2023-10-05 13:44:04 +0800

    add logger and more iteration

--- jobs/jobs/spiders/jobs_spider.py
+++ jobs/jobs/spiders/jobs_spider.py
@@ -4,16 +4,36 @@ import scrapy
 class JobsSpiderSpider(scrapy.Spider):
     name = "jobs_spider"
     allowed_domains = ["linkedin.com"]
-    #base_urls = ["http://linkedin.com/"]
-    location = "Philippines"
-    keywords = ["software development","data and analytics","dasign and ui/ux","testing and quality assurance","networking and infrastructure"]
+    # base_urls = ["http://linkedin.com/"]
+    location = ["Philippines"]
+    keywords = [
+        "software development","data and analytics","dasign and ui/ux",
+        "testing and quality assurance","networking and infrastructure"
+        ]
+    
+    PAGES = 5
+    
+
     start_urls = ["https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?location=Philippines&keywords=" + str(keyword) for keyword in keywords ]
 
     def start_requests(self):
-        for keyword in self.keywords:
-            yield scrapy.Request(url="https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?location=Philippines&keywords=" + str(keyword), callback=self.parse, meta={'keyword': keyword})
+        for location in self.location:
+            for keyword in self.keywords:
+                for page in range(self.PAGES):
+                    start = page * 25 # each page contains 25 jobs
+                    url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?location={location}&keywords={keyword}&start={start}"
+                    yield scrapy.Request(
+                        url= url, 
+                        callback=self.parse, 
+                        meta={'location' : location,'keyword': keyword, 'page' : page}
+                    )
 
     def parse(self, response):
+        location = response.meta['location']
+        keyword = response.meta['keyword']
+        page = response.meta['page']
+
+
         jobs = response.css('div.job-search-card')
 
         for job in jobs:
@@ -23,8 +43,20 @@ class JobsSpiderSpider(scrapy.Spider):
                 'title' : job.css('h3::text').get().strip(),
                 'company' : job.css('a.hidden-nested-link::text').get().strip(),
                 'company_link' : job.css('a.hidden-nested-link::attr(href)').get(),
-                'date' : job.css('time::attr(datetime)').get()
+                'date' : job.css('time::attr(datetime)').get(),
             }
             yield item
 
+            # Log a message for each successful job scraped
+            log_message = f"Scraped job - Title: {item['title']}, Company: {item['company']}"
+            self.log(log_message)
+        
+        log_message = f"location: {location}, keyword: {keyword}, page: {page}, url: {response.url}" 
+        self.log_to_file(log_message)
+
         pass
+
+
+    def log_to_file(self, message):
+        with open('log.txt', 'a') as f:
+            f.write(message + '\n')
